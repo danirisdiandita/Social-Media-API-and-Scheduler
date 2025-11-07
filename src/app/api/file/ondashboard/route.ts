d1d72a41455af9e3db26600ebf376e2d342302c6
danirisdiandita <normadani2016@gmail.com>
2025-11-07 20:28:49 +0700

    update Fri Nov  7 20:28:49 WIB 2025

--- src/app/api/file/ondashboard/route.ts
+++ src/app/api/file/ondashboard/route.ts
@@ -1,4 +1,3 @@
-export const runtime = "nodejs";
 import { NextRequest, NextResponse } from "next/server";
 import crypto from "crypto";
 import { PutObjectCommand, S3Client, S3ServiceException } from "@aws-sdk/client-s3";
